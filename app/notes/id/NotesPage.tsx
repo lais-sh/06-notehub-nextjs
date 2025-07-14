9b1202f8a20283ae29f31ee01da5cf5bb2c98d4d
lais-sh <lithshamok@gmail.com>
2025-07-14 10:50:25 +1000

    new

---
+++ app/notes/id/NotesPage.tsx
@@ -0,0 +1,22 @@
+import { HydrationBoundary, dehydrate } from '@tanstack/react-query';
+import { getQueryClient } from '@/lib/queryClient';
+import { fetchNotes } from '@/lib/api';
+import NotesClient from '../Notes.client';
+
+export default async function NotesPage() {
+  const queryClient = getQueryClient();
+
+  // SSR-запит
+  await queryClient.prefetchQuery({
+    queryKey: ['notes', { page: 1 }],
+    queryFn: () => fetchNotes({ page: 1 }),
+  });
+
+  const dehydratedState = dehydrate(queryClient);
+
+  return (
+    <HydrationBoundary state={dehydratedState}>
+      <NotesClient />
+    </HydrationBoundary>
+  );
+}
