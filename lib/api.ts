046ee267ea7b22ff723d4152602e947720ffaa8d
lais-sh <lithshamok@gmail.com>
2025-07-14 14:54:28 +1000

    new

--- lib/api.ts
+++ lib/api.ts
@@ -1,34 +1,15 @@
 import axios from 'axios';
-import type { Note, FetchNotesResponse } from '@/types/note';
 
-const BASE_URL = 'https://notehub-public.goit.study/api';
+const API_TOKEN = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
 
-const TOKEN = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
-
-if (!TOKEN) {
-  console.warn('⚠️ NOTEHUB API token is missing. Check your .env file.');
+if (!API_TOKEN) {
+  throw new Error('Missing API token.');
 }
 
-const axiosInstance = axios.create({
-  baseURL: BASE_URL,
+export const http = axios.create({
+  baseURL: 'https://notehub-public.goit.study/api',
   headers: {
-    Authorization: `Bearer ${TOKEN}`,
+    Authorization: `Bearer ${API_TOKEN}`,
     'Content-Type': 'application/json',
   },
 });
-
-export async function fetchNotes({ page, search = '' }: { page: number; search?: string }): Promise<FetchNotesResponse> {
-  const params = {
-    page,
-    perPage: 12,
-    ...(search.trim() !== '' && { search: search.trim() }),
-  };
-
-  const response = await axiosInstance.get<FetchNotesResponse>('/notes', { params });
-  return response.data;
-}
-
-export async function fetchNoteById(id: number): Promise<Note> {
-  const response = await axiosInstance.get<Note>(`/notes/${id}`);
-  return response.data;
-}
